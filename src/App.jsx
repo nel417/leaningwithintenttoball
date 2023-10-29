683277af3d098e52ded58a5f91115394344e8442
nickL <n1ck@hey.com>
2023-10-28 21:07:58 -0500

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import Navbar from './components/Navbar'
 import Bloglist from './components/Bloglist'
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Navbar />
     <Bloglist />
+    <Analytics />
      </>
   )
 }
