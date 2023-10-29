500b86600133c6779e37514c5f88911fbaf2169c
nickL <n1ck@hey.com>
2023-10-28 21:20:14 -0500

    a4

--- src/analytics.js
+++ src/analytics.js
@@ -1,3 +1,7 @@
 import { inject } from '@vercel/analytics';
-
-inject();
+// import some helper that is exposed by your current framework to determine the right mode manually
+import { dev } from '$app/environment';
+ 
+inject({
+  mode: dev ? 'development' : 'production',
+});
